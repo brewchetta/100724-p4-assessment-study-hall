924139339a945ed0f43825d95dc8b8578d7c4a34
brewchetta <chett.tiller@gmail.com>
2024-12-10 11:11:32 -0500

    post request

--- server/app.py
+++ server/app.py
@@ -2,11 +2,50 @@
 
 from flask import request
 from config import app, db
-from models import db
+from models import Landlord
 
 @app.get('/')
 def index():
     return "Hello world"
+
+
+# POST REQUEST TO MAKE A NEW LANDLORD
+@app.post('/landlords')
+def create_new_landlord():
+    # 1. parse the body
+    body = request.json
+
+    try:
+
+        # 2. create a new landlord instance using the parsed body
+        new_landlord = Landlord(
+            associated_llcs=body.get('associated_llcs'), 
+            violations=body.get('violations'), 
+            currently_in_litigation=body.get('currently_in_litigation')
+        )
+
+        # 3. add and commit the new landlord
+        db.session.add( new_landlord )
+        db.session.commit()
+
+        # 4. transform landlord into a dictionary and return the dictionary in the response
+        landlord_dict = {
+            "id": new_landlord.id,
+            "associated_llcs": new_landlord.associated_llcs,
+            "violations": new_landlord.violations,
+            "currently_in_litigation": new_landlord.currently_in_litigation
+        }
+
+        return landlord_dict, 201
+    
+    # 5. return an error message for errors
+    except Exception as error:
+
+        return {
+            "status": 400, 
+            "message": "Something went really wrong... do you have an associated_llcs?",
+            "error_text": str(error)
+        }, 400
 
 
 # RUN ##########################
--- server/models.py
+++ server/models.py
@@ -2,4 +2,13 @@
 # from sqlalchemy_serializer import SerializerMixin
 from config import db
 
-# MODELS GO HERE
+
+class Landlord(db.Model):
+
+    __tablename__ = 'landlords_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    associated_llcs = db.Column(db.String, nullable=False)
+    violations = db.Column(db.String)
+    currently_in_litigation = db.Column(db.Boolean, default=True)
+    
