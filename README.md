8246914b3ee7cdc870650a97215d94e6d9ea0bb8
brewchetta <chett.tiller@gmail.com>
2024-12-10 09:50:35 -0500

    tweaks for 100724 flask api

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Learning Goals
 
-- Adding database constraints
+- Recap on building models / migrating databases
 
-- Using the sqlalchemy `validates` decorator
+- Retrieving database data with a GET request
 
-- Handling validations in routes
+- Creating new data with a POST request
 
 ## Getting Started
 
@@ -17,22 +17,9 @@ pipenv install
 pipenv shell
 cd server
 
+BUILD YOUR MODEL(S)
+
 flask db init
 flask db migrate -m "initialize database"
 flask db upgrade
 ```
-
-## Exercises
-
-Add these validations:
-
-1. The `year_joined` must be between 1970 and the current year.
-
-2. Username cannot include naughty words like 'heck', 'frack', or 'bish'.
-
-3. Add a validation so that `phone_number` includes 10 characters (exclude international numbers for now).
-    Bonus: The `phone_number` should also reformat to remove dashes before validating
-    Bonus: The `phone_number` should throw an error if any alphabetical characters are included (use regex)
-
-4. Address must include the words 'Street', 'Avenue', or 'Road'.
-    Bonus: Add a validation so addresses must include a 5 digit zipcode at the end (HINT: you can use a slice to get the last X characters)...
--- server/app.py
+++ server/app.py
@@ -2,77 +2,11 @@
 
 from flask import request
 from config import app, db
-from models import db, User
+from models import db
 
 @app.get('/')
 def index():
     return "Hello world"
-
-
-
-# USER ROUTES ################################
-
-
-@app.get('/users')
-def all_users():
-    return [u.to_dict() for u in User.query.all()], 200
-
-
-@app.get('/users/<int:id>')
-def user_by_id(id):
-    user = User.query.where(User.id == id).first()
-    if user:
-        return user.to_dict(), 200
-    else:
-        return {'error': 'Not found'}, 404
-    
-
-@app.post('/users')
-def post_user():
-    try:
-        user = User(
-            username=request.json.get('username'),
-            email=request.json.get('email'),
-            address=request.json.get('address'),
-            phone_number=request.json.get('phone_number'),
-            age=request.json.get('age'),
-            vip=request.json.get('vip'),
-            year_joined=request.json.get('year_joined')
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
-    except sqlalchemy.exc.IntegrityError as error:
-        return { 'error': 'Invalid data' }, 400
-    except ValueError as error:
-        return { 'error': str(error) }, 400
-
-
-@app.patch('/users/<int:id>')
-def patch_user_by_id(id):
-    user = User.query.where(User.id == id).first()
-    if user:
-        try:
-            for key in request.json:
-                setattr(user, key, request.json[key])
-            db.session.add(user)
-            db.session.commit()
-            return user.to_dict(), 202
-        except:
-            return { 'error': 'Invalid data' }, 400
-    else:
-        return {'error': 'Not found'}, 404
-    
-
-@app.delete('/users/<int:id>')
-def delete_user_by_id(id):
-    user = User.query.where(User.id == id).first()
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        return {}, 204
-    else:
-        return {'error': 'Not found'}, 404
 
 
 # RUN ##########################
